40aec0e7d21b3c104262f051b90b5583e4afd872
gaotranhuy <huy07kt1@gmail.com>
2026-07-01 21:24:16 +0700

    Create site.ts

---
+++ constants/site.ts
@@ -0,0 +1,25 @@
+export const siteConfig = {
+  name: "Gạo Trần Huy",
+
+  shortName: "Gạo Trần Huy",
+
+  description:
+    "Chuyên cung cấp gạo ngon, gạo đặc sản, gạo ST25, gạo lứt, gạo nếp và nước mắm Nam Ô tại Đà Nẵng.",
+
+  url: "https://gaotranhuy.vn",
+
+  locale: "vi_VN",
+
+  keywords: [
+    "gạo đà nẵng",
+    "gạo trần huy",
+    "gạo st25",
+    "gạo ngon",
+    "gạo quê",
+    "nước mắm nam ô"
+  ],
+
+  phone: "0900000000",
+
+  email: "contact@gaotranhuy.vn"
+}
