11d70ae8a2bcffee8c6ca7bec7392a6405807074
gaotranhuy <huy07kt1@gmail.com>
2026-07-02 12:35:11 +0700

    Update site.ts

--- data/site.ts
+++ data/site.ts
@@ -11,14 +11,14 @@ export const siteSettings: SiteSettings = {
 };
 
 export const contactInfo: ContactInfo = {
-  phone: '0901 234 567',
-  zalo: '0901234567',
-  email: 'lienhe@gaotranhuy.vn',
-  address: '123 Đường Nông Sản, Phường An Lạc, Quận Bình Tân, TP. Hồ Chí Minh',
+  phone: '0931.55555.1',
+  zalo: '0931555551',
+  email: 'huy07kt@gmail.com',
+  address: '26 Cầm Bá Thước. Phường Hoà Cường. Tp. Đà Nẵng',
   workingHours: '7:00 - 20:00 (Tất cả các ngày trong tuần)',
   mapEmbed:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.419!2d106.595!3d10.732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTDCsDQzJzU1LjIiTiAxMDbCsDM1JzQyLjAiRQ!5e0!3m2!1svi!2s!4v1700000000000',
-  facebook: 'https://facebook.com/gaotranhuy',
-  tiktok: 'https://tiktok.com/@gaotranhuy',
+  facebook: 'https://www.facebook.com/share/172h9krRwX/?mibextid=wwXIfr',
+  tiktok: 'https://www.tiktok.com/@huy07kt?_r=1&_t=ZS-97gnmLX82kv',
   youtube: 'https://youtube.com/@gaotranhuy',
 };
