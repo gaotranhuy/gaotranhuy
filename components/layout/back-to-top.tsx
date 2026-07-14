d2987678269d7e9869c50e8133f39d8222cecb65
gaotranhuy <huy07kt1@gmail.com>
2026-07-14 08:57:39 +0700

    Update back-to-top.tsx

--- components/layout/back-to-top.tsx
+++ components/layout/back-to-top.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import Image from 'next/image';
 import { ArrowUp } from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { contactInfo } from '@/data/site';
@@ -29,9 +30,14 @@ export function BackToTop() {
           'animate-scale-in'
         )}
       >
-        <svg viewBox="0 0 24 24" className="h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 2C6.48 2 2 5.94 2 10.8c0 2.77 1.46 5.23 3.75 6.82L5 22l4.36-2.27c.84.2 1.72.3 2.64.3 5.52 0 10-3.94 10-8.8S17.52 2 12 2zm-1.5 13.5l-3-3 1.06-1.06 1.94 1.94 4.44-4.44L15.94 10l-5.44 5.5z"/>
-        </svg>
+        <Image
+          src="/icons/zalo.svg"
+          alt="Zalo"
+          width={24}
+          height={24}
+          className="h-6 w-6"
+          priority
+        />
       </a>
 
       {/* Back to top */}
